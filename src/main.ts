ea6689c7702aaa62b7d74fabd4a883d560d4e72b
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-03-11 00:19:11 +0200

    Add time and state request handling for rooms

--- src/main.ts
+++ src/main.ts
@@ -179,6 +179,18 @@ io.on('connection', socket => {
 		}
 	)
 
+	socket.on(ACTIONS.REQUEST_TIME_AND_STATE, ({ roomID }) => {
+		console.log(`[INFO] Client ${socket.id} requested time and state for room: ${roomID}`)
+		socket.to(roomID).emit(ACTIONS.REQUEST_TIME_AND_STATE, { socketID: socket.id })
+	})
+
+	socket.on(ACTIONS.SEND_TIME_AND_STATE, ({ socketID, time, isPlaying }) => {
+		console.log(
+			`[INFO] Client ${socket.id} sending time and state to ${socketID}. Time: ${time}, isPlaying: ${isPlaying}`
+		)
+		io.to(socketID).emit(ACTIONS.SYNC_STATE, { time, isPlaying })
+	})
+
 	socket.on(ACTIONS.REQUEST_PARTICIPANT_INFO, ({ roomID }) => {
 		console.log(`[INFO] Client ${socket.id} requested participant info for room: ${roomID}`)
 		socket.to(roomID).emit(ACTIONS.REQUEST_PARTICIPANT_INFO, { requesterId: socket.id })
